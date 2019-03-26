93d17bf8df2d4922341726dad742fba9a0b34cf3
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2019-03-26 11:21:26 +0700

    Done make react and redux simple title of article

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import index from "./js/index"
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App.jsx";
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
