31ba3195e20202217c1b65e1d4e5cbeee4d5482a
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2019-03-26 14:30:58 +0700

    done implement redux thunk

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import List from "./List.jsx";
 import Form from "./Form.jsx";
+import Post from "./Posts.jsx";
 
 
 const App = () => (
@@ -12,6 +13,10 @@ const App = () => (
         <div className="col-md-4 offset-md-1">
             <h2>Add a new article</h2>
             <Form />
+        </div>
+        <div className="col-md-4 offset-md-1">
+            <h2>API posts</h2>
+            <Post />
         </div>
     </div>
 );
