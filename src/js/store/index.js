7d6600f73eb674a1930df8d0bd02c065225e0338
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2019-03-26 15:24:42 +0700

    done implement redux saga

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -2,13 +2,19 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
 import thunk from "redux-thunk";
+import apiSaga from "../sagas/api-sagas";
+import createSagaMiddleware from "redux-saga";
+
+const initialSagaMiddleWare = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 compose;
 
 const store = createStore(
     rootReducer,
-    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk, initialSagaMiddleWare))
     );
+
+initialSagaMiddleWare.run(apiSaga);
 
 export default store;
